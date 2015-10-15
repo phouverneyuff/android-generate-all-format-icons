c7eecb9e6eba79a7529cc985a21a1cb36a148c23
phouverneyuff <ph_ouverney@id.uff.br>
2015-10-14 21:52:13 -0300

    Improving script with validations and parametrized

--- generate.sh
+++ generate.sh
@@ -3,10 +3,25 @@ INKSCAPE="/usr/bin/inkscape"
 
 OPTS="--without-gui --export-background-opacity=0"
 
-SVG=icon.svg
+if [ -z $1 ]; then
+    echo "File not found"
+    exit 0
+fi
 
-DEST_LAUCHER_ICONS=./laucher_icons
-[ -d $DEST_LAUCHER_ICONS ] || mkdir $DEST_LAUCHER_ICONS
+FILE=$1
+NAMEFILE=$(basename $FILE | cut -d. -f1)
+EXTENSION=$(basename $FILE | cut -d. -f2)
+
+
+if [ $EXTENSION != 'svg' ]; then
+    echo "You need inform a svg file "
+    exit 0
+fi
+
+SVG=$1
+
+DEST_LAUNCHER_ICONS=./launcher_icons
+[ -d $DEST_LAUNCHER_ICONS ] || mkdir $DEST_LAUNCHER_ICONS
 
 DEST_ACTION_ICONS=./actionbar_dialog_tab_icons
 [ -d $DEST_ACTION_ICONS ] || mkdir $DEST_ACTION_ICONS
@@ -21,32 +36,32 @@ DEST_GOOGLE_PLAY_ICONS=./google_play
 [ -d $DEST_GOOGLE_PLAY_ICONS ] || mkdir $DEST_GOOGLE_PLAY_ICONS
 
 #launcher icons
-"$INKSCAPE" -w48 $OPTS --export-png=$DEST_LAUCHER_ICONS/Icon_Android48.png $SVG
-"$INKSCAPE" -w72 $OPTS --export-png=$DEST_LAUCHER_ICONS/Icon_Android72.png $SVG
-"$INKSCAPE" -w96 $OPTS --export-png=$DEST_LAUCHER_ICONS/Icon_Android96.png $SVG
-"$INKSCAPE" -w144 $OPTS --export-png=$DEST_LAUCHER_ICONS/Icon_Android144.png $SVG
-"$INKSCAPE" -w192 $OPTS --export-png=$DEST_LAUCHER_ICONS/Icon_Android192.png $SVG
+"$INKSCAPE" -w48 $OPTS --export-png=$DEST_LAUNCHER_ICONS/"$NAMEFILE"_launcher_48.png $SVG
+"$INKSCAPE" -w72 $OPTS --export-png=$DEST_LAUNCHER_ICONS/"$NAMEFILE"_launcher_72.png $SVG
+"$INKSCAPE" -w96 $OPTS --export-png=$DEST_LAUNCHER_ICONS/"$NAMEFILE"_launcher_96.png $SVG
+"$INKSCAPE" -w144 $OPTS --export-png=$DEST_LAUNCHER_ICONS/"$NAMEFILE"_launcher_144.png $SVG
+"$INKSCAPE" -w192 $OPTS --export-png=$DEST_LAUNCHER_ICONS/"$NAMEFILE"_launcher_192.png $SVG
 
 #action icons
-"$INKSCAPE" -w24 $OPTS --export-png=$DEST_ACTION_ICONS/Icon_Android24.png $SVG
-"$INKSCAPE" -w36 $OPTS --export-png=$DEST_ACTION_ICONS/Icon_Android36.png $SVG
-"$INKSCAPE" -w48 $OPTS --export-png=$DEST_ACTION_ICONS/Icon_Android48.png $SVG
-"$INKSCAPE" -w72 $OPTS --export-png=$DEST_ACTION_ICONS/Icon_Android72.png $SVG
-"$INKSCAPE" -w96 $OPTS --export-png=$DEST_ACTION_ICONS/Icon_Android96.png $SVG
+"$INKSCAPE" -w24 $OPTS --export-png=$DEST_ACTION_ICONS/"$NAMEFILE"_24.png $SVG
+"$INKSCAPE" -w36 $OPTS --export-png=$DEST_ACTION_ICONS/"$NAMEFILE"_36.png $SVG
+"$INKSCAPE" -w48 $OPTS --export-png=$DEST_ACTION_ICONS/"$NAMEFILE"_48.png $SVG
+"$INKSCAPE" -w72 $OPTS --export-png=$DEST_ACTION_ICONS/"$NAMEFILE"_72.png $SVG
+"$INKSCAPE" -w96 $OPTS --export-png=$DEST_ACTION_ICONS/"$NAMEFILE"_96.png $SVG
 
 #notifications icons
-"$INKSCAPE" -w22 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/Icon_Android22.png $SVG
-"$INKSCAPE" -w33 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/Icon_Android33.png $SVG
-"$INKSCAPE" -w44 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/Icon_Android44.png $SVG
-"$INKSCAPE" -w66 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/Icon_Android66.png $SVG
-"$INKSCAPE" -w88 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/Icon_Android88.png $SVG
+"$INKSCAPE" -w22 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/"$NAMEFILE"_notifications_22.png $SVG
+"$INKSCAPE" -w33 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/"$NAMEFILE"_notifications_33.png $SVG
+"$INKSCAPE" -w44 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/"$NAMEFILE"_notifications_44.png $SVG
+"$INKSCAPE" -w66 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/"$NAMEFILE"_notifications_66.png $SVG
+"$INKSCAPE" -w88 $OPTS --export-png=$DEST_NOTIFICATIONS_ICONS/"$NAMEFILE"_notifications_88.png $SVG
 
 #small_contextual_icons
-"$INKSCAPE" -w16 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/Icon_Android16.png $SVG
-"$INKSCAPE" -w24 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/Icon_Android24.png $SVG
-"$INKSCAPE" -w32 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/Icon_Android32.png $SVG
-"$INKSCAPE" -w48 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/Icon_Android48.png $SVG
-"$INKSCAPE" -w64 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/Icon_Android64.png $SVG
+"$INKSCAPE" -w16 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/"$NAMEFILE"_small_16.png $SVG
+"$INKSCAPE" -w24 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/"$NAMEFILE"_small_24.png $SVG
+"$INKSCAPE" -w32 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/"$NAMEFILE"_small_32.png $SVG
+"$INKSCAPE" -w48 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/"$NAMEFILE"_small_48.png $SVG
+"$INKSCAPE" -w64 $OPTS --export-png=$DEST_SMALL_CONTEXTUAL_ICONS/"$NAMEFILE"_small_64.png $SVG
 
 #google_play
-"$INKSCAPE" -w512 $OPTS --export-png=$DEST_GOOGLE_PLAY_ICONS/Icon_Android512.png $SVG
+"$INKSCAPE" -w512 $OPTS --export-png=$DEST_GOOGLE_PLAY_ICONS/"$NAMEFILE"_google_play_512.png $SVG
